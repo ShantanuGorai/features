519391c8fecac52e013188c9ee983dedf821c983
ShantanuGorai <shantanugorai013@gmail.com>
2026-08-23 00:38:40 +0530

    Start repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# features
+
+[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-czwfugj9)
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import BentoGrid01 from "@/components/ui/bento-grid-01";
+
+function App() {
+  return <BentoGrid01 />;
+}
+
+export default App;
